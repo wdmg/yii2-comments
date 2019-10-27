a3dc338244f38fcc002bdb7e2b3dcfc2459356cd
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2019-10-28 00:30:15 +0200

    Added some translations

--- messages/ru-RU/app/modules/comments.php
+++ messages/ru-RU/app/modules/comments.php
@@ -3,6 +3,10 @@
 return [
     'Comments' => "Комментарии",
     'Tree comments system' => "Древовидная система комментариев",
+
+
+    'No comments available' => "Нет доступных для отображения комментариев",
+    'An error occurred while retrieving the comments list' => "Произошла ошибка при получении списка комментариев",
 ];
 
 ?>
